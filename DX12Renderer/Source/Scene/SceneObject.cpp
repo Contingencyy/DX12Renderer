543b3601930172096fcc95ccf99ea3e764e375a0
Justin Kujawa <34250026+Contingencyy@users.noreply.github.com>
2022-11-04 18:02:45 +0100

    Fix for multiple components of the same type
    [FIX] Fixed ImGui push IDs with multiple components that have the same name/collapsed header

--- DX12Renderer/Source/Scene/SceneObject.cpp
+++ DX12Renderer/Source/Scene/SceneObject.cpp
@@ -46,6 +46,7 @@ void SceneObject::Render()
 
 void SceneObject::OnImGuiRender()
 {
+	// Push ID for this current scene object name
 	ImGui::PushID(m_Name.c_str());
 
 	if (ImGui::CollapsingHeader(m_Name.c_str()))
@@ -54,12 +55,20 @@ void SceneObject::OnImGuiRender()
 		{
 			if (m_Components[i].size() > 0)
 			{
+				// Push ID for the current component ID
 				ImGui::PushID(i);
 				ImGui::Indent(20.0f);
 
+				uint32_t compIndex = 0;
 				for (auto& component : m_Components[i])
 				{
+					// Push ID for the current component index in the component vector
+					ImGui::PushID(compIndex);
+
 					component->OnImGuiRender();
+
+					ImGui::PopID();
+					compIndex++;
 				}
 
 				ImGui::Unindent(20.0f);
